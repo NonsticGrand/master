3382f7e603495d6339dd57fd790fef94e15af421
NonsticGrand <63715404+NonsticGrand@users.noreply.github.com>
2020-06-24 22:01:55 +0900

    Add files via upload

--- april 29/backup/unit1.pas
+++ april 29/backup/unit1.pas
@@ -15,10 +15,11 @@ type
     Men: TMainMenu;                                     //Ниже представлены поля
     MenuItem1: TMenuItem;
     MenuItem2: TMenuItem;
-    FSaveAs: TMenuItem;
     MenuItem4: TMenuItem;
     MenuItem5: TMenuItem;
     fOpenDialog: TOpenDialog;
+    MenuItem6: TMenuItem;
+    FSaveAs1: TMenuItem;
     Run: TButton;
     Edit1: TEdit;
     Number: TLabel;
@@ -26,12 +27,12 @@ type
     TSG: TStringGrid;
     procedure Button1Click(Sender: TObject);                 //Начало объявления методов
     procedure FormCreate(Sender: TObject);
-    procedure MenuItem1Click(Sender: TObject);
+    procedure FSaveAs1Click(Sender: TObject);
     procedure MenuItem2Click(Sender: TObject);
-    procedure FSaveAsClick(Sender: TObject);
+    procedure MenuItem3Click(Sender: TObject);
     procedure MenuItem4Click(Sender: TObject);
     procedure MenuItem5Click(Sender: TObject);
-    procedure RezultsChange(Sender: TObject);                //Конец объявления методов
+    procedure MenuItem6Click(Sender: TObject);             //Конец объявления методов
   private
 
   public
@@ -39,7 +40,7 @@ type
   end;
 
 var
-  Pz1: TPz1;   f,a: real; n:integer;
+  Pz1: TPz1;   n:integer; c:calcul2; a:calcul;
   fFilePath:string = '';
 
 implementation                                                   //ниже представлено определение методов
@@ -55,56 +56,37 @@ begin
   TSG.RowCount:=2;//строки(Y)
 end;
 
+procedure TPz1.FSaveAs1Click(Sender: TObject);
+ begin
+   save_as_s(fFilePath);
+  save_otv(c,fFilePath,n);
+  end;
+
+
+procedure TPz1.MenuItem2Click(Sender: TObject);
+begin
+  open;
+end;
+
+
 procedure TPz1.Button1Click(Sender: TObject);
 begin
-  a:=StrToFloat(Edit1.Text);
-  Calculate(a,f);
-  TSG.Cells[1,n]:=FloatToStr(n);
-  TSG.Cells[1,n]:=FloatToStr(a);
-  TSG.Cells[2,n]:=FloatToStr(f);
+  c[n].x:=StrToFloat(Edit1.Text);
+  Calculate(c,n);
+  TSG.Cells[0,n]:=FloatToStr(n);
+  TSG.Cells[1,n]:=FloatToStr(c[n].x);
+  TSG.Cells[2,n]:=FloatToStr(c[n].y);
   TSG.RowCount:=n+2;
   n:=n+1;
 end;
 
 
-
-procedure TPz1.MenuItem1Click(Sender: TObject);
-var s:string;
-  f1:text;
+procedure TPz1.MenuItem6Click(Sender: TObject);
 begin
- if fOpenDialog.Execute then
-     begin
-         fFilePath:=fOpenDialog.FileName;
-         AssignFile(f1,fFilePath);
-         reset(f1);
-         readln(f1,s);
-         Edit1.Text:=s;
-         closeFile(f1);
-     end;
-end;
+ save(fFilePath);
+ save_otv(c,fFilePath,n);
+ end;
 
-procedure TPz1.MenuItem2Click(Sender: TObject);
-begin
- if fFilePath='' then FSaveAs.Click
-else  save_otv(a,f,fFilePath);
-end;
-
-procedure TPz1.FSaveAsClick(Sender: TObject);
-var s:string;
-begin
-  if fFilePath='' then fsavedialog.FileName:='Новый документ.txt'
-  else fsavedialog.FileName:=fFilePath;
-
-     if fSaveDialog.Execute then
-       begin
-
-         if extractfileext(fSaveDialog.FileName)='' then
-            fFilePath:=fSaveDialog.FileName+'.txt'
-        else fFilePath:=fSaveDialog.FileName;
-
-        save_otv(a,f,fFilePath);
-       end;
-end;
 
 procedure TPz1.MenuItem4Click(Sender: TObject);
 begin
@@ -116,10 +98,6 @@ begin
  ShowMessage('Давыдов Никита'+#13#10+#13#10+'Задача №57в'+#13#10+'Дано действительное число a. Вычислить f(a), если:'+#13#10+'(x^2+4*x+5) при x<=2'+#13#10+'1/(x^2+4*x+5) в противном случае'+#13#10+#13#10+'http://bgu-chita.ru/zadachnik/Glava02/index02.htm#z57');
 end;
 
-procedure TPz1.RezultsChange(Sender: TObject);
-begin
-
-end;
 
 end.
 
--- april 29/unit2.pas
+++ april 29/unit2.pas
@@ -2,32 +2,95 @@ unit Unit2;
 
 {$mode objfpc}{$H+}
 
+
+
+
+
+
 interface
-Procedure Calculate(var a,f:real);
-procedure save_otv(var a,f:real; var filename: string);
+type
+calcul = record
+x,y:real;
+end;
+
+calcul2= array[1..128] of calcul;
+
+
+Procedure Calculate(var c:calcul2; n:integer);
+procedure save_otv(var c:calcul2; var filename: string; var n:integer);
+Procedure save(var ffilename:string);
+Procedure open();
+Procedure save_as_s(var ffilename:string);
 
 implementation
 uses
   Classes, SysUtils, Unit1;
 
-Procedure Calculate(var a,f:real);
+Procedure Calculate(var c:calcul2; n:integer);
 begin
-  if a > 2 then
-     f:=1/(a*a+4*a+5)
+  if c[n].x > 2 then
+     c[n].y:=1/(c[n].x*c[n].x+4*c[n].x+5)
      else
-     f:=a*a+4*a+5;
-  f:=Trunc(f);
+     c[n].y:=c[n].x*c[n].x+4*c[n].x+5;
+  c[n].y:=Trunc(c[n].y);
 end;
 
-procedure save_otv(var a,f:real; var filename: string);
+procedure save_otv(var c:calcul2; var filename: string; var n:integer);
   var
-    fl: text;
+    f1: text;
     s:string;
+    i:integer;
+ begin
+ AssignFile(f1,filename);
+ append(f1);
+ for i:=1 to (n-1) do
+ begin
+ s:='Номер опыта= '+IntToStr(i)+' Входные данные= '+FloatToStr(c[i].x)+' Результат= '+FloatToStr(c[i].y);
+ Write(f1,s);
+ writeln(f1);
+ end;
+ writeln(f1);
+ closefile(f1);
+ end;
+
+
+Procedure save(var ffilename:string);
+begin
+  if not Pz1.fOpenDialog.Execute then exit;
+  if Pz1.fOpenDialog.Execute then
+ fFilename:=Pz1.fOpenDialog.FileName;
+end;
+
+
+Procedure open();
+  var s:string;
+    f1:text;
   begin
-       assign(fl, filename);
-       rewrite(fl);
-       s:='Введённые данные: ' + FloatToStr(a) +'Ответ: ' + FloatToStr(f);
-       writeln(fl,s);
-       close(fl);
+   if not Pz1.fOpenDialog.Execute then exit;
+   if Pz1.fOpenDialog.Execute then
+       begin
+           fFilePath:=Pz1.fOpenDialog.FileName;
+           AssignFile(f1,fFilePath);
+           reset(f1);
+           readln(f1,s);
+           Pz1.Edit1.Text:=s;
+           closeFile(f1);
+    end;
   end;
+
+
+Procedure save_as_s(var ffilename:string);
+  var s:string;
+   begin
+     if not Pz1.fSaveDialog.Execute then exit;
+     ffilename:=Pz1.fOpenDialog.FileName;
+     if ffilename='' then Pz1.fsavedialog.FileName:='Новый документ.txt'
+     else Pz1.fsavedialog.FileName:=fFilePath;
+        if Pz1.fSaveDialog.Execute then
+          begin
+            if extractfileext(Pz1.fSaveDialog.FileName)='' then
+               ffilename:=Pz1.fSaveDialog.FileName+'.txt'
+           else ffilename:=Pz1.fSaveDialog.FileName;
+            end;
+          end;
 end.
